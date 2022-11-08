bc8be2afca4b3fc70d2de766a786a0ba237b2569
EricBollar <esbollar@gmail.com>
2022-11-07 19:58:57 -0500

    ground

--- minecraft/src/App.js
+++ minecraft/src/App.js
@@ -1,5 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 import { Sky } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import { Ground } from "./components/Ground"
 import './App.css';
 
 function App() {
@@ -8,6 +10,9 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]}/>
         <ambientLight intensity={0.5}/>
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
     </div>
   );
--- minecraft/src/images/textures.js
+++ minecraft/src/images/textures.js
@@ -8,11 +8,11 @@ import {
     woodImg
 } from './images'
 
-const dirtTexture = new TextureLoader(dirtImg);
-const logTexture = new TextureLoader(logImg);
-const grassTexture = new TextureLoader(grassImg);
-const glassTexture = new TextureLoader(glassImg);
-const woodTexture = new TextureLoader(woodImg);
+const dirtTexture = new TextureLoader().load(dirtImg);
+const logTexture = new TextureLoader().load(logImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
 
 export {
     dirtTexture,
