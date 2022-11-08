4ce00a4508aa2c1412c780349518e2af63f97ef1
EricBollar <esbollar@gmail.com>
2022-11-07 19:36:19 -0500

    sky

--- minecraft/src/App.js
+++ minecraft/src/App.js
@@ -1,13 +1,14 @@
 import { Canvas } from '@react-three/fiber'
+import { Sky } from '@react-three/drei'
 import './App.css';
 
 function App() {
   return (
-    <>
-      <div>Outside Canvas</div>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas>
+        <Sky sunPosition={[100, 100, 20]}/>
       </Canvas>
-    </>
+    </div>
   );
 }
 
