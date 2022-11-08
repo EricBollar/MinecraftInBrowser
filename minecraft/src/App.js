5760b4e8752808a15e709196557a26e80d75cfbd
EricBollar <esbollar@gmail.com>
2022-11-07 19:31:48 -0500

    boilerplate

---
+++ minecraft/src/App.js
@@ -0,0 +1,14 @@
+import { Canvas } from '@react-three/fiber'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>Outside Canvas</div>
+      <Canvas>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
